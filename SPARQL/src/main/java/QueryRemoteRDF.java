0a33999dc0811ab748d2e2ba6ceda574e0df4aff
Benjamin <adolphe906@gmail.com>
2021-02-28 20:36:55 +0100

    req 10 11 12

--- SPARQL/src/main/java/QueryRemoteRDF.java
+++ SPARQL/src/main/java/QueryRemoteRDF.java
@@ -52,43 +52,26 @@ public class QueryRemoteRDF {
                 "SELECT ?f " +
                 "FROM " + link
                 + " WHERE { " +
-                "?f rdf:type dc:title" +
+                "?f dc:title ?o" +
                 " }";
 
-
-        /*String queryMailOfTimsFriends =
-
-                "PREFIX foaf:  <http://xmlns.com/foaf/0.1/> "
-                        + "PREFIX card: <http://www.w3.org/People/Berners-Lee/card#> " + "SELECT ?homepage "
-                        + "FROM <http://dig.csail.mit.edu/2008/webdav/timbl/foaf.rdf> " + "WHERE {"
-                        + "       card:i foaf:knows ?known ." + "       ?known foaf:homepage ?homepage . }";
-
-        String queryFederatedDataSet =
-
-                "PREFIX movie: <http://data.linkedmdb.org/resource/movie/> "
-                        + "PREFIX foaf: <http://xmlns.com/foaf/0.1/> "
-                        + "PREFIX dbpedia: <http://dbpedia.org/ontology/> " + "SELECT * "
-                        + "FROM <http://dig.csail.mit.edu/2008/webdav/timbl/foaf.rdf>  " + "WHERE {" + "  "
-                        + "  SERVICE <http://data.linkedmdb.org/sparql> {"
-                        + "    <http://data.linkedmdb.org/resource/film/675> movie:actor ?actor ."
-                        + "    ?actor movie:actor_name ?actor_name" + "  }  "
-                        + "  BIND(STRLANG(?actor_name_imdb, \"en\") AS ?actor_name_en) " + "   "
-                        + "  SERVICE <http://dbpedia-live.openlinksw.com/sparql?timeout=2000> { " + "    SELECT * "
-                        + "    WHERE {  ?actor2 a                  foaf:Person ; "
-                        + "                     foaf:name          ?actor_name_en ;  "
-                        + "                     dbpedia:birthDate  ?birth_date . "
-                        + "             FILTER(STR(?actor_name_en) = ?actor_name)" + "    } LIMIT 2" + "  }"
-                        + "} LIMIT 2";
-
-        String ListRSSFeedClasses =
-
-                "PREFIX vCard: <http://www.w3.org/2001/vcard-rdf/3.0#> " + "PREFIX rss: <http://purl.org/rss/1.0/> "
-                        + "PREFIX foaf: <http://xmlns.com/foaf/0.1/> "
-                        + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " + "" + "SELECT * "
-                        + "FROM <http://www.w3.org/2001/sw/SW-FAQ-feed.rdf> " + " WHERE { "
-                        + " ?channel rdf:type rss:channel ." + " ?channel rss:items ?itemset ."
-                        + " ?itemset ?sequenceNumber ?item ." + " ?item rdf:type ?type ." + "} ";*/
-
+        String req11 = "PREFIX dc:<http://purl.org/dc/elements/1.1/>" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                " " +
+                "SELECT DISTINCT ?s " +
+                "FROM " + link
+                + " WHERE { " +
+                "?s dc:subject ?o " +
+                " }" +
+                "LIMIT 1";
+        String req12 = "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
+                + " PREFIX dc: <http://purl.org/dc/elements/1.1/>"
+                + " PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+                + " PREFIX rss: <http://purl.org/rss/1.0/>"
+                + "PREFIX swq: <http://www.w3.org/2001/sw/SW-FAQ#>"
+                + "SELECT ?o"
+                + " FROM <http://www.w3.org/2001/sw/SW-FAQ-feed.rdf>"
+                + "WHERE { ?s dc:date ?o}";
         /**
          *
          * Notice that we do not set a database - this is a class for querying a remote
@@ -102,7 +85,7 @@ public class QueryRemoteRDF {
          *
          */
 
-        Query query = QueryFactory.create(req10);
+        Query query = QueryFactory.create(req12);
 
         QueryExecution qexec = QueryExecutionFactory.create(query);
 
